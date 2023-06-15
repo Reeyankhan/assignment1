fbbd38594c7b7ecc253d043bfd11d456f953dfb7
Reeyankhan <136440172+Reeyankhan@users.noreply.github.com>
2023-06-15 11:22:54 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,7 @@
+name=input("enter your name")
+print(name)
+m1=int(input("enter your marks 1"))
+m2=int(input("enter your marks 2"))
+m3=int(input("enter your marks 3"))
+avg = (m1+m2+m3)/3
+print("average mrks",avg)
